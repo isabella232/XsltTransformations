80e0a3e94d1a7e6d4b0a2e1dd27d86d4884eaa58
jachymovicS4B <v.jachymovic@s4b.sk>
2020-05-20 14:31:28 +0200

    Modification of Invoices.xsd file due to new Slovak Word template

--- WordprocessingML/Templates/Invoice/transform.bat
+++ WordprocessingML/Templates/Invoice/transform.bat
@@ -2,3 +2,4 @@
 ..\..\bin\Wml2Ew.exe "Invoice_EU_EN.xml" "urn:eway:document-schemas:Invoice" "Invoice (VAT)" "Carts" "en" "2" "0"
 ..\..\bin\Wml2Ew.exe "Invoice_NO.xml" "urn:eway:document-schemas:Invoice" "Faktura (MVA)" "Carts" "no" "2" "0"
 ..\..\bin\Wml2Ew.exe "Invoice_USD_EN.xml" "urn:eway:document-schemas:Invoice" "Invoice" "Carts" "en" "2" "0"
+..\..\bin\Wml2Ew.exe "Invoice_SK.xml" "urn:eway:document-schemas:Invoice" "Faktúra" "Carts" "sk" "2" "0"
