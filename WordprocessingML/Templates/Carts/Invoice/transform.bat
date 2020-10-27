bce63d624be5c4334a7cb45ec23adc91b6195ccb
rstefko <roman.stefko@eway-crm.com>
2020-10-27 14:06:12 +0100

    Fixed paths

--- WordprocessingML/Templates/Carts/Invoice/transform.bat
+++ WordprocessingML/Templates/Carts/Invoice/transform.bat
@@ -1,7 +1,7 @@
 chcp 65001
 
-..\..\bin\Wml2Ew.exe "Invoice_CZ.xml" "urn:eway:document-schemas:Invoice" "Faktura" "Carts" "cs" "2" "0"
-..\..\bin\Wml2Ew.exe "Invoice_EU_EN.xml" "urn:eway:document-schemas:Invoice" "Invoice (VAT)" "Carts" "en" "2" "0"
-..\..\bin\Wml2Ew.exe "Invoice_NO.xml" "urn:eway:document-schemas:Invoice" "Faktura (MVA)" "Carts" "no" "2" "0"
-..\..\bin\Wml2Ew.exe "Invoice_USD_EN.xml" "urn:eway:document-schemas:Invoice" "Invoice" "Carts" "en" "2" "0"
-..\..\bin\Wml2Ew.exe "Invoice_SK.xml" "urn:eway:document-schemas:Invoice" "Faktúra" "Carts" "sk" "2" "0"
+..\..\..\bin\Wml2Ew.exe "Invoice_CZ.xml" "urn:eway:document-schemas:Invoice" "Faktura" "Carts" "cs" "2" "0"
+..\..\..\bin\Wml2Ew.exe "Invoice_EU_EN.xml" "urn:eway:document-schemas:Invoice" "Invoice (VAT)" "Carts" "en" "2" "0"
+..\..\..\bin\Wml2Ew.exe "Invoice_NO.xml" "urn:eway:document-schemas:Invoice" "Faktura (MVA)" "Carts" "no" "2" "0"
+..\..\..\bin\Wml2Ew.exe "Invoice_USD_EN.xml" "urn:eway:document-schemas:Invoice" "Invoice" "Carts" "en" "2" "0"
+..\..\..\bin\Wml2Ew.exe "Invoice_SK.xml" "urn:eway:document-schemas:Invoice" "Faktúra" "Carts" "sk" "2" "0"
--- WordprocessingML/Templates/Projects/ProjectSheet/transform.bat
+++ WordprocessingML/Templates/Projects/ProjectSheet/transform.bat
@@ -1,3 +1,3 @@
-﻿..\..\bin\Wml2Ew.exe "ProjektovyList.xml" "urn:eway:document-schemas:export-wml-project" "Projektový list" "Projects" "cs" "2" "0"
-..\..\bin\Wml2Ew.exe "ProjectSheet.xml" "urn:eway:document-schemas:export-wml-project" "Project Sheet" "Projects" "en" "2" "0"
-..\..\bin\Wml2Ew.exe "ProjectSheet_no.xml" "urn:eway:document-schemas:export-wml-project" "Prosjektark" "Projects" "no" "2" "0"
+﻿..\..\..\bin\Wml2Ew.exe "ProjektovyList.xml" "urn:eway:document-schemas:export-wml-project" "Projektový list" "Projects" "cs" "2" "0"
+..\..\..\bin\Wml2Ew.exe "ProjectSheet.xml" "urn:eway:document-schemas:export-wml-project" "Project Sheet" "Projects" "en" "2" "0"
+..\..\..\bin\Wml2Ew.exe "ProjectSheet_no.xml" "urn:eway:document-schemas:export-wml-project" "Prosjektark" "Projects" "no" "2" "0"
